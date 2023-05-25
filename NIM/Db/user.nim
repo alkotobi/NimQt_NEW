ef9716e963f5e6361b90d931372f687b32753aae
merhab <merhab@gmail.com>
2023-05-25 19:32:01 +0100

    user dbconn

--- NIM/Db/user.nim
+++ NIM/Db/user.nim
@@ -24,26 +24,36 @@ proc getCaptions*(self:User):seq[string]=
   result = @["id","name","login","pass"]
 proc getFieldsCount*(self:User):int=
   result = 4
-proc getFieldsNames(self:User):seq[string]=
+proc getFieldsNames*(self:User):seq[string]=
   result = @["id","name","login","pass"]
 
-proc sqlCreateTable(self:User,engine = "SQLITE"):string=
+proc createTable*(self:User,db:DbConn,engine = "SQLITE"):bool=
+  var str = ""
   if engine == "SQLITE":
-    result = "CREATE TABLE IF NOT EXISTS user(id INTEGER PRIMARY KEY AUTOINCREMENT,name TEXT,login TEXT,pass TEXT);"
+    str = "CREATE TABLE IF NOT EXISTS user(id INTEGER PRIMARY KEY AUTOINCREMENT,name TEXT,login TEXT,pass TEXT);"
   elif engine == "MYSQL":
-    result = "CREATE TABLE IF NOT EXISTS user(id INTEGER PRIMARY KEY AUTO_INCREMENT,name TEXT,login TEXT,pass TEXT);"
+    str = "CREATE TABLE IF NOT EXISTS user(id INTEGER PRIMARY KEY AUTO_INCREMENT,name TEXT,login TEXT,pass TEXT);"
+  return db.tryExec(str.sql())
 
-proc sqlInsert(self:User):string=
-  result = "INSERT INTO user (name,login,pass) VALUES (?,?,?);"
-proc sqlUpdate(self:User,id:int):string=
-  result = &"UPDATE user set name = ? , login = ? , pass = ? WHERE id ={id}"
-
-
-
-
-var user = newUser(newMVariant(1,"id"),newMVariant("merhab","name"),newMVariant("merhab","login"),newMVariant("123","pass"))
-echo user
+proc insert*(self:User,db:DbConn):int=
+  var id= db.tryInsertID(sql"INSERT INTO user (name,login,pass) VALUES (?,?,?);",self.name.getStringValue(),self.login.getStringValue(),self.pass.getStringValue()).int()
+  self.id.setVal(newMVariant(id))
+  result = id
+proc update*(self:User,db:DbConn):bool=
+  var str = &"UPDATE user set name = ? , login = ? , pass = ? WHERE id =?"
+  result = db.tryExec(str.sql,self.name.getStringValue(),self.login.getStringValue(),self.pass.getStringValue(),self.id.getIntValue())
+  
+var user = newUser(newMVariant(1,"id"),newMVariant("nour","name"),newMVariant("abi","login"),newMVariant("567","pass"))
+var name = user.name.getStringValue()
+echo name
+var login = user.login.getStringValue()
+echo login
+var pass = user.pass.getStringValue()
+echo pass
 let db = open("mytest.db", "", "", "")
-db.exec(sqlCreateTable(user).sql())
+# echo db.tryInsertID(sql"INSERT INTO user (name,login,pass) VALUES (?,?,?);",name,login,pass)
+let v=user.createTable(db)
+let id = user.insert(db)
+
 
 
