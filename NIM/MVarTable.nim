beebffca3ff854ef9e20181d41a4961d6bb5fd2b
merhab <merhab@gmail.com>
2023-06-24 06:30:16 +0800

    test in china

--- NIM/MVarTable.nim
+++ NIM/MVarTable.nim
@@ -8,7 +8,7 @@ proc newMVarTable(data:seq[MRecord]):MVarTable=
   new result
   initMTable(result.MTable(),data)
 proc getData*(self:MVarTable,row,column:int):ref MVariant=
-  result =  self.data[row].fields[column]
+  result =  self.data[row].fields[column] 
 proc setData*(self:MVarTable,row,column:int,val:ref MVariant)=
   ## just change the value inside the MVariant
   var v = self.data[row].fields[column]
